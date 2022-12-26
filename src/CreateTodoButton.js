8a374de4e064ac1af6aac4b6b208c596bb48fd34
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-26 18:15:49 -0300

    Manejo de eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton() {
+    const onclickButton = (msg) => {
+        alert(msg)
+    };
+
     return (
-        <button className="CreateTodoButton">+</button>
+        <button
+            className="CreateTodoButton"
+
+            onClick={() => onclickButton('Aqui se deberia abrir el modal')}
+        // onClick={() => console.log('Clic')}
+        >
+            +
+        </button >
     );
 }
 
