7f0bb55e91e45dec12e3eb1aed25c2ae5c537d53
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-26 19:46:46 -0300

    Completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -39,6 +39,25 @@ function App() {
     });
   }
 
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+
+  };
+
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+
+  };
+
 
   return (
     <React.Fragment>
@@ -58,6 +77,9 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+
           />
         ))}
 
