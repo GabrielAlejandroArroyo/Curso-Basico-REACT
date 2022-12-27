f745eb8b0e0bdfd11900f6634941618aa3c01e86
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-27 18:07:16 -0300

    useContext, utilizando el react hook

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,41 +7,42 @@ import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI() {
+    // Hook userContext
+    const {
+        error,
+        loading,
+        searchedTodos,
+        completeTodo,
+        deleteTodo,
+    } = React.useContext(TodoContext);
+
+
     return (
         <React.Fragment>
             <TodoCounter />
             <TodoSearch />
-            <TodoContext.Consumer>
-                {({
-                    error,
-                    loading,
-                    searchedTodos,
-                    completeTodo,
-                    deleteTodo,
-                }) => (
-                    <TodoList>
-                        {/* Colocacion de distintos estados */}
-                        {error && <p>Desesperate hubo un error ......</p>}
-                        {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando los datos */}
-                        {loading && <p>Estamos cargando, no desesperes ......</p>}
-                        {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para crear el primer TODO */}
-                        {(!loading && !searchedTodos.length) && <p>!!Crea tu primer Todo</p>}
+
+            <TodoList>
+                {/* Colocacion de distintos estados */}
+                {error && <p>Desesperate hubo un error ......</p>}
+                {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando los datos */}
+                {loading && <p>Estamos cargando, no desesperes ......</p>}
+                {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para crear el primer TODO */}
+                {(!loading && !searchedTodos.length) && <p>!!Crea tu primer Todo</p>}
 
 
-                        {searchedTodos.map(todo => (
-                            // Para solucionar el Warning: Each children in a list should have a unic "key" prop, tenemos que utilizar key={todo.text}  
-                            <TodoItem
-                                key={todo.text}
-                                text={todo.text}
-                                completed={todo.completed}
-                                onComplete={() => completeTodo(todo.text)}
-                                onDelete={() => deleteTodo(todo.text)}
-                            />
-                        ))}
+                {searchedTodos.map(todo => (
+                    // Para solucionar el Warning: Each children in a list should have a unic "key" prop, tenemos que utilizar key={todo.text}  
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
 
-                    </TodoList>
-                )}
-            </TodoContext.Consumer>
+            </TodoList>
             <CreateTodoButton />
         </React.Fragment>
     );
