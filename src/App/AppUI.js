f8e606902d78462d24098fd3826ffc502c75f363
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-12-27 17:42:26 -0300

    React Context: estado compartido

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,53 +1,47 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
-function AppUI({
-    loading,
-    error,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-}) {
+function AppUI() {
     return (
         <React.Fragment>
-            <TodoCounter
-                total={totalTodos}
-                completed={completedTodos}
-            />
-            <TodoSearch
-                // Envio las propiedades
-                searchValue={searchValue}
-                setSearchValue={setSearchValue}
-            />
-            <TodoList>
-                {/* Colocacion de distintos estados */}
-                {error && <p>Desesperate hubo un error ......</p>}
-                {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando los datos */}
-                {loading && <p>Estamos cargando, no desesperes ......</p>}
-                {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para crear el primer TODO */}
-                {(!loading && !searchedTodos.length) && <p>!!Crea tu primer Todo</p>}
+            <TodoCounter />
+            <TodoSearch />
+            <TodoContext.Consumer>
+                {({
+                    error,
+                    loading,
+                    searchedTodos,
+                    completeTodo,
+                    deleteTodo,
+                }) => (
+                    <TodoList>
+                        {/* Colocacion de distintos estados */}
+                        {error && <p>Desesperate hubo un error ......</p>}
+                        {/* Mostramos un mensaje de cargando, cuando la aplicación está cargando los datos */}
+                        {loading && <p>Estamos cargando, no desesperes ......</p>}
+                        {/* Si terminó de cargar y no existen TODOs, se muestra un mensaje para crear el primer TODO */}
+                        {(!loading && !searchedTodos.length) && <p>!!Crea tu primer Todo</p>}
 
 
-                {searchedTodos.map(todo => (
-                    // Para solucionar el Warning: Each children in a list should have a unic "key" prop, tenemos que utilizar key={todo.text}  
-                    <TodoItem
-                        key={todo.text}
-                        text={todo.text}
-                        completed={todo.completed}
-                        onComplete={() => completeTodo(todo.text)}
-                        onDelete={() => deleteTodo(todo.text)}
-                    />
-                ))}
+                        {searchedTodos.map(todo => (
+                            // Para solucionar el Warning: Each children in a list should have a unic "key" prop, tenemos que utilizar key={todo.text}  
+                            <TodoItem
+                                key={todo.text}
+                                text={todo.text}
+                                completed={todo.completed}
+                                onComplete={() => completeTodo(todo.text)}
+                                onDelete={() => deleteTodo(todo.text)}
+                            />
+                        ))}
 
-            </TodoList>
+                    </TodoList>
+                )}
+            </TodoContext.Consumer>
             <CreateTodoButton />
         </React.Fragment>
     );
